02e5ca1800c7a4070cdaf56905c66fbc808ce0a6
Dobromir Kovachev <dobromir.mail@gmail.com>
2018-12-11 09:32:32 +0200

    Rework of data retrievel of courses (after the last update on 09.12.2018)

--- course_scraper/course_scraper/spiders/scrape-coursera.py
+++ course_scraper/course_scraper/spiders/scrape-coursera.py
@@ -11,7 +11,6 @@ class CourseraSpider(scrapy.Spider):
 
     def parse(self, response):
         sel = scrapy.Selector(response)
-        # store all the information into *.csv files
         academy_csv = open("academy.csv", "a+")
         fieldnames = ['platform', 'title', 'description', 'logo', 'banner']
         academy_writer = csv.DictWriter(academy_csv, fieldnames=fieldnames)
@@ -19,7 +18,7 @@ class CourseraSpider(scrapy.Spider):
         fieldnames =  [
                       'platform', 'issuer', 'title', 'about', 'purpose',
                       'tutors', 'level', 'duration', 'language', 'stars',
-                      'logo', 'banner', 'link'
+                      'logo', 'link'
                       ]
         course_writer = csv.DictWriter(course_csv, fieldnames=fieldnames)
         specialization_csv = open("specialization.csv", "a+")
@@ -58,6 +57,7 @@ class CourseraSpider(scrapy.Spider):
     def academy_details(self, response):
         academy_writer = response.meta['academy_writer']
         course_writer = response.meta['course_writer']
+        specialization_writer = response.meta['specialization_writer']
         title = response.xpath('//div[@class="rc-PartnerHeader"]/div[2]/div/div[2]/h1/text()').extract()
         description = response.xpath('//div[@class="rc-PartnerHeader"]/div[2]/div/div[2]/p/text()').extract()
         logo = response.xpath('//div[@class="rc-PartnerHeader"]/div[2]/div/div[1]/div/img/@src').extract()
@@ -66,7 +66,8 @@ class CourseraSpider(scrapy.Spider):
         description = self.return_val(description)
         logo = self.return_val(logo)
         banner = self.return_val(banner)
-        academy_writer.writerow({'title': str(title),
+        academy_writer.writerow({'platform': "Coursera",
+                                 'title': str(title),
                                  'description': str(description),
                                  'logo': str(logo),
                                  'banner': str(banner)})
@@ -85,38 +86,23 @@ class CourseraSpider(scrapy.Spider):
         course_writer = response.meta['course_writer']
         specialization_writer = response.meta['specialization_writer']
         course_logo = response.meta['course_logo']
-        title = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[2]/div[1]/main/div[1]/h1/span/text()').extract()
+        title = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/h1/text()').extract()
         is_specialization = True if len(response.xpath("//*[contains(@id, 'courses')]")) else False
         if is_specialization:
             print("SPECIALIZATION")
             # 'platform', 'issuer', 'title', 'about', 'courses'
+            about = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/text()').extract()
 
         else:
-            print("COURSE")
             issuer = about = table = level = duration = language = stars = ""
-            # 'platform', 'issuer', 'title', 'about', 'purpose',
-            # 'tutors', 'level', 'duration', 'language', 'stars',
-            # 'logo', 'banner', 'link'
-            banner = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]//@style').re_first(r'url\(([^\)]+)')
-            issuer = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/span/text()').extract()
-            about = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/p/text()').extract()
-            table = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[5]/table/tbody/tr/td/span/text()').extract()
-            table_elements = ['Level', 'Language', 'Commitment', 'User Ratings']
-            for item in table_elements:
-                item_index = [i for i, s in enumerate(table) if item in s]
-                if item != 'User Ratings':
-                    item_value = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[5]/table/tbody/tr['+str(item_index[0])+']/td[2]/text()').extract()
-                else:
-                    item_value = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[5]/table/tbody/tr['+str(item_index[0])+']/td[2]/div/div/span[1]/text()')[1].extract()
+            issuer = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/img/@title').extract()
+            about = response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/text()').extract()
+            path_parameters = '//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/div[1]/'
+            level = self.return_val(response.xpath(path_parameters + 'div[3]/div[2]/h4/text()').extract())
+            duration = self.return_val(response.xpath(path_parameters + 'div[4]/div[2]/h4/text()').extract())
+            language = self.return_val(response.xpath(path_parameters + 'div[5]/div[2]/h4/text()').extract())
+            stars = self.return_val(response.xpath('//div[@id="rendered-content"]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/span/text()').extract())
 
-                if item == 'Level':
-                    level = item_value
-                elif item == 'Language':
-                    language = item_value
-                elif item == 'Commitment':
-                    duration = item_value
-                elif item == 'User Ratings':
-                    stars = item_value
             course_writer.writerow({'platform': 'Coursera',
                                     'issuer': str(issuer),
                                     'title': str(title),
@@ -126,7 +112,4 @@ class CourseraSpider(scrapy.Spider):
                                     'language': str(language),
                                     'stars': str(stars),
                                     'logo': str(course_logo),
-                                    'banner': str(banner),
                                     'link': str(response.url)})
-
-#ldks;
